ccb4261b6d7ff1ef947dedd2e4cef728e7842690
IT24200741 <it24200741@my.sliit.lk>
2026-09-19 00:27:09 +0530

    fix: dynamically adjust NatleLogo SVG viewBox based on showTagline to prevent squishing and alignment issues

--- src/components/NatleLogo.tsx
+++ src/components/NatleLogo.tsx
@@ -27,8 +27,8 @@ export default function NatleLogo({
       />
       {/* 2. Pristine Vector SVG Logo: [ N ] [ A ] [ T ] [ L ] [ E ] */}
       <motion.svg
-        viewBox="0 0 460 160"
-        className="h-9 sm:h-10 md:h-11 lg:h-12 w-auto overflow-visible"
+        viewBox={showTagline ? "0 0 460 160" : "10 10 390 115"}
+        className="h-full w-auto overflow-visible"
         style={{ maxHeight: "100%", maxWidth: "100%", width: "auto" }}
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
