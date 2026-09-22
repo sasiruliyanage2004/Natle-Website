bfdb2aef23a3cb06a4fc4954471ee32403de522f
IT24200741 <it24200741@my.sliit.lk>
2026-09-23 00:21:39 +0530

    design: upgrade typography to Plus Jakarta Sans for the ultimate premium Silicon Valley aesthetic, completely replacing standard fonts

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -1,16 +1,15 @@
-﻿import localFont from "next/font/local";
+import { Plus_Jakarta_Sans } from "next/font/google";
 
-export const display = localFont({
-  src: "./Inter.ttf",
+export const display = Plus_Jakarta_Sans({
+  subsets: ["latin"],
   variable: "--font-display",
   display: "swap",
+  weight: ["400", "500", "600", "700", "800"],
 });
 
-export const body = localFont({
-  src: [
-    { path: "./Inter.ttf", style: "normal" },
-    { path: "./Inter-Italic.ttf", style: "italic" },
-  ],
+export const body = Plus_Jakarta_Sans({
+  subsets: ["latin"],
   variable: "--font-body",
   display: "swap",
+  weight: ["400", "500", "600", "700"],
 });
